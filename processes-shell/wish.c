ed4fcb809a9b1ab93fdfa553dd4a80faefc44883
Kesler Mathieu <kmat5@icloud.com>
2023-09-15 19:35:33 -0400

    fleshing out shell logic

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -11,12 +11,14 @@ int main(int argc, char* argv[])
 {
     input->stdin_line = NULL;
     input->stdin_len = 0;
+	 char *PATH = "/bin/";
 
     printf("wish>");
     fflush( stdout );
     while (1){
       // obtain user cmd
 		getline(input->stdin_line, input->stdin_len, stdin);
+
 		// there's no need to process cmds if input is empty
 		if (input->stdin_len > 0){
 			int rc = fork();
@@ -24,8 +26,25 @@ int main(int argc, char* argv[])
 				fprintf(stderr, "fork failed\n");
 			}
 			else if (rc == 0){
-				// execute cmds
-				// TODO: must dynamically parse inputs
+				char binPath[input->stdin_len + strlen(PATH)];
+				strcat(binPath, PATH);
+				strcat(binPath, input->stdin_line);
+			 
+				// determine if binary exists
+				if (access(binPath, F_OK) == 0){
+
+					// determine executable permissions for binary
+					if (access(binPath, X_OK) == 0){
+						// TODO: must parse input for args
+						execv(binPath, " ");
+					}
+					else{
+						perror("Unable to execute binary\n");
+					}
+				}
+				else{
+					perror("Command not found\n");
+				}
 			}
 			else{
 				// wait for child to finish and print shell prompt again
