b62893b91f1b211325a1fcee1cf616ed7be4a670
Kesler Mathieu <kmat5@icloud.com>
2023-11-24 22:15:02 -0500

    new commits

--- concurrency-pzip/gzip.c
+++ concurrency-pzip/gzip.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <string.h>
 // STEPS:
 // (1) - Read in File as a string, S
 // (2) - Pass S into a function that returns number of unique chars, K
@@ -15,3 +17,37 @@
 // in parallel, each thread will compute their diffs = {4,3,2} along with S[b] = {a,b,c} thus
 // Unordered_Result = 2c4a3b, as order output is not guaranteed with threads
 // Hence for every S[b], we get index(S[b]), and write {Unordered_Result[index(S[b]) - 1], S[b]} to ensure proper ordering
+
+int cout_unique_chars(char *s){
+    int cout = 1;
+    for(int i = 0; i < strlen(s) - 1; i++){
+        if(s[i] != s[i+1]){
+            cout++;
+        }
+    }
+    return cout;
+}
+
+// int** get_transitional_indicies(char *s){
+  
+// }
+
+int main(int argc, char* argv[])
+{
+	// if (argc == 2){
+	// 	FILE *file = fopen(argv[1], "r");
+    // 	if(file == NULL){
+    //     	printf("Unable to open file: %s\n", argv[1]);
+    //     	exit(-1);
+    // 	}
+    //     else{
+
+    //     }
+	// }
+    // else{
+    //     printf("File path not supplied. Exiting...");
+    //     	exit(-1);
+    // }
+    printf("%i\n", cout_unique_chars("abcdefghijklmnopqrstuvqxyz"));
+    printf("%i\n", cout_unique_chars("aaaabbbcc"));
+}
